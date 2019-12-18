a4a9e7a0ddc9cb0c2322b3571cb1d8bec47e2e01
pparmeggiani <parmeggianipedro@gmail.com>
2019-12-18 15:33:43 -0300

    Small REST api

--- http/main.go
+++ http/main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"goApi/http/handler"
+	"goApi/platform/newsfeed"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	feed := newsfeed.New()
+
 	r := gin.Default()
 
-	r.GET("/ping", handler.PingGet)
+	r.GET("/ping", handler.PingGet())
+	r.GET("/getFeed", handler.NewsFeedGet(feed))
+	r.POST("/postFeed", handler.NewsFeedPost(feed))
 
 	r.Run()
 }
